1287e8d9548ae707e8bca6e290654d8bc9736dd0
Haacked <haacked@gmail.com>
2013-04-08 16:48:19 -0700

    Initial implementation of Fibonacci sequence
    Am I doing it right?

---
+++ FizzBonacci/FizzBonaccinator.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzinator
+    {
+        public IEnumerable<int> Generate(int count)
+        {
+            yield return 1;
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 5;
+            yield return 8;
+            yield return 13;
+            yield return 21;
+            yield return 34;
+            yield return 55;
+        }
+    }
+}
+
---
+++ FizzBonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        const int totalCount = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to get the first 10 Fibbonacci numbers!");
+            Console.ReadKey();
+
+            var fizzBuzzinator = new FizzBuzzinator();
+            var sequence = fizzBuzzinator.Generate(totalCount);
+            Console.WriteLine();
+            Console.WriteLine(String.Join(", ", sequence.ToArray()));
+            Console.WriteLine();
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+            Console.WriteLine("QUITTER!");
+        }
+    }
+}
